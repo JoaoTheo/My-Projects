cbc0c825f6c1a078ced9243398fccade14b9cd2b
Theo Souza <joatheocarrarasouza@gmail.com>
2025-03-14 09:24:32 -0300

    Função Escolher curso

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 function entrarNoCurso() {
   console.log("Entrou no Curso");
 }
+function sairDoCurso() {
+  console.log("Saiu do curso");
+}
+
+function escolherCurso() {
+  console.log("Escolheu o Curso");
+}
